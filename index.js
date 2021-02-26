d1336c81b54be60657688951851fce4bb2b2306c
Pia-N <pianurmi@hotmail.fi>
2021-02-26 19:34:12 +0200

    3.18*: Phonebook database step6

--- index.js
+++ index.js
@@ -69,28 +69,34 @@ app.get("/", (request, res) => {
   res.send("<h1>Hello World!</h1>");
 })
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(persons => {
     response.json(persons)
   })
+    .catch(error => next(error))
 })
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id)
 
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      }
+      else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.get("/info", (request, response) => {
-  const howMany = persons.length.toString()
-  const info = `Phonebook has information of ${howMany} people.<br> ${new Date()}</br>`
+app.get("/info", (request, response, next) => {
+  Person.countDocuments({})
+    .then(count => {
+      response.send(`Phonebook has information of ${count} people.<br> ${new Date()}</br>`)
+    })
+    .catch(error => next(error))
 
-  response.send(info)
 })
 
 //DELETE
@@ -104,7 +110,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 //POST
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body
   console.log(body, "body")
 
@@ -133,10 +139,44 @@ app.post("/api/persons", (request, response) => {
   person.save().then(response => {
     console.log(`Added ${person.name} number ${person.number} to phonebook.`)
   })
+    .catch(error => next(error))
 
   persons = persons.concat(person)
   response.json(person)
 })
+//PUT
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+// olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
